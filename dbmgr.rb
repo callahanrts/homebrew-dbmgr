4e5abee4b6975cce9a9a52a2f1fef28c7f18cbd8
Cody <callahanrts@gmail.com>
2017-10-19 21:00:12 -0700

    bump version

--- dbmgr.rb
+++ dbmgr.rb
@@ -2,8 +2,8 @@ class Dbmgr < Formula
 
   desc "Backup and restore development databases"
   homepage "https://github.com/callahanrts/dbmgr"
-  url "https://github.com/callahanrts/dbmgr/archive/v0.1.3.tar.gz"
-  sha256 "6ee04bb1c4c1b5eb5a68103073ab9bac7ea5427b973ef2447d9c9bf44b8add68"
+  url "https://github.com/callahanrts/dbmgr/archive/v0.1.4.tar.gz"
+  sha256 "88bffaf25c48bc72e939ed5c90d5a19fde53d67555ed28f0c19245908817288b"
 
   head "https://github.com/callahanrts/dbmgr.git"
   bottle :unneeded
